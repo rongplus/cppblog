641bbd948634153a55595e738c62e444f0cb317f
rongplus <rong.hr.plus@gmail.com>
2022-01-20 15:56:01 -0500

    const

--- file/read.cpp
+++ file/read.cpp
@@ -2,11 +2,17 @@
 #include <fstream> 
 #include <string>
 #include <unordered_set>
+#include <vector>
 using namespace std;
+
+const string g_file_path2 = R"(c:\data\)";
+const string g_file_path1 = R"(C:\Users\metar\Downloads\)";
+const string g_file_path = g_file_path2;
+
 
 void test()
 {
-    ifstream ff("C:\\Users\\metar\\Downloads\\5000_total.txt");
+    ifstream ff(g_file_path + "5000_total.txt");
     if (ff.is_open() == false)
     {
         std::cout << "file error" << std::endl;
@@ -17,7 +23,7 @@ void test()
     unordered_set<string> uset[total];
     size_t nfile=0;
     size_t index=0;
-    ofstream ffin("C:\\Users\\metar\\Downloads\\total--1.txt");
+    ofstream ffin(g_file_path+"total--1.txt");
     while( getline(ff,line))
     {
         if (line.length() !=109)
@@ -28,67 +34,99 @@ void test()
 
 }
 
+void deal(int n)
+{
+     ifstream ff(g_file_path+ "5000_" +to_string(n) + ".txt");
+        if (ff.is_open() == false)
+        {
+            std::cout << "file error - " << n << std::endl;
+            return;
+        }
+    const int total = 100;
+    unordered_set<string> uset[total];
+     string line;
+     int index = 0;
+
+     while( getline(ff,line))
+     {
+         uset[index++].emplace(line);
+         if(index==total)
+             index=0;
+     }
+
+     ofstream ffin(g_file_path+"5000_result_" +to_string(n) + ".txt");
+
+      for (auto &it: uset)
+       {
+            for(auto &s: it)
+            {
+                ffin << s << endl;
+            }
+           // uset[n].swap( unordered_set<string> ());
+        }
+
+        ffin.close();
+}
+
 int main()
 {
    // test();
   //  return 0;
-    ifstream ff("C:\\Users\\metar\\Downloads\\5000.txt");
+  const int total = 28;
+//    for(int n=0; n< total; ++n)
+//     {
+//        deal(n);
+//        cout << "done file " << n << endl;
+//     }
+
+//     return 0;
+
+    ifstream ff(g_file_path + "5000.txt");
     if (ff.is_open() == false)
     {
         std::cout << "file error" << std::endl;
         return 0;
     }
     string line;
-    const int total = 28;
+    
     unordered_set<string> uset[total];
     size_t nfile=0;
     size_t index=0;
-    ofstream ffin("C:\\Users\\metar\\Downloads\\5000_total.txt");
+   
+    ofstream ffout[total];
+    vector<ofstream> out_list;
+    for(int n=0; n< total; ++n)
+    {
+        out_list.push_back( ofstream (g_file_path+"5000_" +to_string(n) + ".txt"));
+    }   
     while( getline(ff,line))
     {
         if (line.length() < 50)
             continue;
-        uset[index++].emplace(line);
-        if(index==total)
+      out_list[index++] << line << endl;
+        if(index>=total)
             index=0;
-      
-        if (0)
-        {
-        //    ofstream ffin("C:\\Users\\metar\\Downloads\\5000_" +to_string(++nfile) + ".txt");
-           for (int n=0; n< total; n++)
-           {
-                for (auto &it: uset[n])
-                {
-                    ffin << it << endl;
-                }
-                uset[n].swap( unordered_set<string> ());
-           }
-       //     ffin.close();
-            
-            
-           
-
-            
-
-        }
-       
-        //std::cout << line << std::endl;
+        
     }
-// ofstream ffin("C:\\Users\\metar\\Downloads\\5000_" +to_string(++index) + ".txt");
-// ofstream ffin("C:\\Users\\metar\\Downloads\\5000_" +to_string(++nfile) + ".txt");
-           for (int n=0; n< total; n++)
-           {
-               ofstream ffin("C:\\Users\\metar\\Downloads\\5000_" +to_string(n) + ".txt");
-                for (auto &it: uset[n])
-                {
-                    ffin << it << endl;
-                }
-                uset[n].swap( unordered_set<string> ());
-                 ffin.close();
-           }
+
+    cout << "Done read" << endl;
+
+    for( auto &it: out_list)
+    {
+        it.close();
+    }
+    ff.close();
+    for(int n=0; n< total; ++n)
+    {
+       deal(n);
+       cout << "done file " << n << endl;
+    }
+    
+
+     
            
     
-std::cout << "done - remove short" << std::endl;
+    std::cout << "done - remove short" << std::endl;
 
 
 
